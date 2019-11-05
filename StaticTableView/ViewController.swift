7fe261dbf59ec903708dcc75349e0a5d7cfd89b5
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-11-05 17:53:57 -0500

    Initial Commit

---
+++ StaticTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StaticTableView
+//
+//  Created by MacStudent on 2019-11-05.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
